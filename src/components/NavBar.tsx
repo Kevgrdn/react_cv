0569bc55c7b7aa68dfcf59f151cfc2244e133d31
kevin <kevin.grdn37800@gmail.com>
2022-12-27 14:24:26 +0100

    feat: :sparkles: react router dom

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Stack, MenuItem, Menu, AppBar, Toolbar, Typography } from "@mui/material"
+import { Crown } from "lucide-react"
+import { Link } from 'react-router-dom'
+
+
+function NavBar() {
+
+
+    return (
+        <>
+            <AppBar position="fixed" color="primary">
+                <Toolbar>
+                    <Typography variant="h6">
+                        Text
+                    </Typography>
+                    <Typography >
+                        <Link to="/home" >
+                            Home
+                        </Link>
+                        {/* <Link to="/projects" >
+                            About
+                        </Link>
+                        <Link to="/education" >
+                            Contact
+                        </Link> */}
+
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </>
+
+
+
+    )
+}
+
+
+export default NavBar
---
+++ src/routes/routes.config.ts
@@ -0,0 +1,11 @@
+import Portfolio from "../pages/Portfolio/Portfolio";
+
+const routes = [
+    {
+        'name': 'projects',
+        'path': '/projects',
+        'component': Portfolio
+    },
+];
+
+export default routes
