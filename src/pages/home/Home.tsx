7571fe30036d4de162fab08b0d82df6b037dc39e
Dylan Pignolet <dylan.pignolet2@gmail.com>
2022-12-27 15:17:07 +0100

    feat: :art: css home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -3,25 +3,29 @@ import Avatar from '@mui/material/Avatar'
 import Typography from '@mui/material/Typography'
 import { Stack } from '@mui/system'
 import React from 'react'
+import NavBar from '../../components/NavBar'
 
 function Home() {
   return (
-    <Stack>
-      <Stack direction='row' sx={{ margin: 5 }}>
-        <Stack sx={{ margin: 5 }}>
-          <Typography variant='h3'>Hello,</Typography>
-          <Typography variant='h2'>Nom Prénom</Typography>
-          <Typography variant='subtitle1'>Dev Web</Typography>
-          <Button variant="contained">Contact</Button>
-        </Stack>
-        <Stack sx={{ margin: 5, }}>
-          <Avatar alt="avatar" src="/src/assets/avatar.png" />
-        </Stack>
-      </Stack>
+
+    <><NavBar />
       <Stack>
-        Stack technique
+        <Stack direction='row' justifyContent='space-around' sx={{ marginTop: '50px' }}>
+          <Stack alignItems='center'>
+            <Typography variant='h3'>Hello,</Typography>
+            <Typography variant='h2'>Nom Prénom</Typography>
+            <Typography variant='subtitle1'>Dev Web</Typography>
+            <Button variant="contained" sx={{ width: '35%', borderRadius: '1px' }}>Contact</Button>
+          </Stack>
+          <Stack justifyContent='center'>
+            <Avatar alt="avatar" src="/src/assets/avatar.png" sx={{ width: '15rem', height: '15rem' }} />
+          </Stack>
+        </Stack>
+        <Stack sx={{ marginTop: '50px' }}>
+          <Typography variant='h3'>Stack technique</Typography>
+        </Stack>
       </Stack>
-    </Stack>
+    </>
   )
 }
 
