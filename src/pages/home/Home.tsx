a22d1965c761d13c57753df6634b9e011777b918
Dylan Pignolet <dylan.pignolet2@gmail.com>
2022-12-27 14:25:10 +0100

    feat: :sparkles: home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@mui/material'
+import Avatar from '@mui/material/Avatar'
+import Typography from '@mui/material/Typography'
+import { Stack } from '@mui/system'
+import React from 'react'
+
+function Home() {
+  return (
+    <Stack>
+      <Stack direction='row' sx={{ margin: 5 }}>
+        <Stack sx={{ margin: 5 }}>
+          <Typography variant='h3'>Hello,</Typography>
+          <Typography variant='h2'>Nom Prénom</Typography>
+          <Typography variant='subtitle1'>Dev Web</Typography>
+          <Button variant="contained">Contact</Button>
+        </Stack>
+        <Stack sx={{ margin: 5, }}>
+          <Avatar alt="avatar" src="/src/assets/avatar.png" />
+        </Stack>
+      </Stack>
+      <Stack>
+        Stack technique
+      </Stack>
+    </Stack>
+  )
+}
+
+export default Home
