6dc3840dd1543face04555cfbecf7720e8c6c861
Dylan Pignolet <dylan.pignolet2@gmail.com>
2022-12-28 15:49:02 +0100

    feat: :art: ajout stack technique

--- src/api/img.ts
+++ src/api/img.ts
@@ -1,5 +1,22 @@
 import reactIcon from '../assets/react.png'
 import vuejsIcon from '../assets/vuejs.png'
+import nodejsIcon from '../assets/nodejs.png'
+import htmlIcon from '../assets/html.png'
+import cssIcon from '../assets/css.png'
+import javascriptIcon from '../assets/javascript.png'
+import expressIcon from '../assets/express.png'
+import githubIcon from '../assets/github.png'
+import gitIcon from '../assets/git.png'
+import mysqlIcon from '../assets/mysql.png'
+import sequelizeIcon from '../assets/sequelize.png'
+import viteIcon from '../assets/vite.png'
+import mongodbIcon from '../assets/mongodb.png'
+import muiIcon from '../assets/mui.png'
+import typescriptIcon from '../assets/typescript.png'
+import vscodeIcon from '../assets/vscode.png'
+
+
+
 
 export const stackTechniqueImg = [
     {
@@ -9,5 +26,61 @@ export const stackTechniqueImg = [
     {
         icon: vuejsIcon,
         alt: 'vuejs'
-    }
+    },
+    {
+        icon: nodejsIcon,
+        alt: 'nodejs',
+    },
+    {
+        icon: htmlIcon,
+        alt: 'html',
+    },
+    {
+        icon: cssIcon,
+        alt: 'css',
+    },
+    {
+        icon: javascriptIcon,
+        alt: 'javascript',
+    },
+    {
+        icon: expressIcon,
+        alt: 'express',
+    },
+    {
+        icon: gitIcon,
+        alt: 'git',
+    },
+    {
+        icon: githubIcon,
+        alt: 'github',
+    },
+    {
+        icon: mysqlIcon,
+        alt: 'mysql',
+    },
+    {
+        icon: sequelizeIcon,
+        alt: 'sequelize',
+    },
+    {
+        icon: viteIcon,
+        alt: 'vite',
+    },
+    {
+        icon: mongodbIcon,
+        alt: 'mongodb',
+    },
+    {
+        icon: muiIcon,
+        alt: 'mui',
+    },
+    {
+        icon: typescriptIcon,
+        alt: 'typescript',
+    },
+    {
+        icon: vscodeIcon,
+        alt: 'vscode',
+    },
 ]
