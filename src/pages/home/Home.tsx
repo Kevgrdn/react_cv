db928caa640fa2cfcdfa5303615e7960794b21d5
kevin <kevin.grdn37800@gmail.com>
2022-12-29 17:43:32 +0100

    feat: :sparkles: homepage css

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -25,56 +25,52 @@ function Home() {
 
 
   return (
-    <Container maxWidth="lg">
+    <Container disableGutters >
       <NavBar />
+      <Stack sx={{ marginTop: '5rem', }}>
+        <Stack direction='row' justifyContent='center'>
+          <Stack alignItems='center' justifyContent='center'>
+            <Stack>
+              <Typography variant='h3' fontFamily="monospace">Bonjour,</Typography>
+              <Typography variant='h2'>
+                <b>
+                  {`${user.nom} ${user.prenom}`}
+                </b>
+              </Typography>
+            </Stack>
+            <Typography variant='subtitle1'>Développeur FullStack</Typography>
+            <Button variant="contained" type='button' sx={{ borderRadius: '1.5rem' }}>
+              <Stack direction="row" spacing={1}>
+                <Stack>
+                  <Download />
+                </Stack>
+                <Stack>
+                  Télécharger le CV
+                </Stack>
+              </Stack>
+            </Button>
+          </Stack>
+          <Box flexGrow={0.3} />
+          <Stack justifyContent='center'>
+            <Avatar alt="avatar" src="/src/assets/photopro.jpg" sx={{ width: '15rem', height: '15rem' }} />
+          </Stack>
+        </Stack>
+        <Stack sx={{ marginTop: '50px' }} alignItems='center'>
+          <Typography variant='h3'>Stack technique</Typography>
+          <Stack direction='row' spacing={1}>
+            {
+              stackTechniqueImg.map((icon) => {
+                return (<Card key={icon.alt} variant="outlined" sx={{ width: '3.5rem', height: '3.3rem', padding: '0.7rem', backgroundColor: '#F0FFFF', border: '#F0FFFF' }}>
+                  <CardActionArea>
+                    <img alt={icon.alt} src={icon.icon} width='100%' height='100%' />
+                  </CardActionArea>
+                </Card>)
+              })
+            }
+          </Stack>
+        </Stack>
+      </Stack>
     </Container>
-
-    // <Container disableGutters >
-    //   <NavBar />
-    //   <Stack sx={{ marginTop: '5rem', }}>
-    //     <Stack direction='row' justifyContent='center'>
-    //       <Stack alignItems='center' justifyContent='center'>
-    //         <Stack>
-    //           <Typography variant='h3' fontFamily="monospace">Bonjour,</Typography>
-    //           <Typography variant='h2'>
-    //             <b>
-    //               {`${user.nom} ${user.prenom}`}
-    //             </b>
-    //           </Typography>
-    //         </Stack>
-    //         <Typography variant='subtitle1'>Développeur FullStack</Typography>
-    //         <Button variant="contained" type='button' sx={{ borderRadius: '1.5rem' }}>
-    //           <Stack direction="row" spacing={1}>
-    //             <Stack>
-    //               <Download />
-    //             </Stack>
-    //             <Stack>
-    //               Télécharger le CV
-    //             </Stack>
-    //           </Stack>
-    //         </Button>
-    //       </Stack>
-    //       <Box flexGrow={0.3} />
-    //       <Stack justifyContent='center'>
-    //         <Avatar alt="avatar" src="/src/assets/photopro.jpg" sx={{ width: '15rem', height: '15rem' }} />
-    //       </Stack>
-    //     </Stack>
-    //     <Stack sx={{ marginTop: '50px' }} alignItems='center'>
-    //       <Typography variant='h3'>Stack technique</Typography>
-    //       <Stack direction='row' spacing={1}>
-    //         {
-    //           stackTechniqueImg.map((icon) => {
-    //             return (<Card key={icon.alt} variant="outlined" sx={{ width: '3.5rem', height: '3.3rem', padding: '0.7rem', backgroundColor: '#F0FFFF', border: '#F0FFFF' }}>
-    //               <CardActionArea>
-    //                 <img alt={icon.alt} src={icon.icon} width='100%' height='100%' />
-    //               </CardActionArea>
-    //             </Card>)
-    //           })
-    //         }
-    //       </Stack>
-    //     </Stack>
-    //   </Stack>
-    // </Container>
 
   )
 }
